1b5e0fa8c7ebca8990d0d32d8064a46764597221
pranitchandel <chandel.pranit@gmail.com>
2023-01-25 19:21:41 +0530

    api changes

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -29,11 +29,12 @@ router.post("/addTask", async (req, res) => {
 // @desc   get all tasks
 // @access Public
 router.get("/all", async (req, res) => {
+  console.log("Params", req.query);
   try {
-    const tasks = await Task.find();
+    const tasks = await Task.find(req.query).sort({ _id: "desc" });
     res.json(tasks);
   } catch (err) {
-    console.error(err.message);
+    //console.error(err.message);
     res.status(500).send("Server error");
   }
 });
@@ -43,13 +44,14 @@ router.get("/all", async (req, res) => {
 // @access Public
 router.delete("/delete/:task_id", async (req, res) => {
   try {
-    res.send(req.params.task_id);
+    //res.send(req.params.task_id);
     const foundTask = await Task.findById(req.params.task_id);
-    console.log(foundTask);
+    //console.log(foundTask);
     if (!foundTask) {
       console.log("error");
     } else {
       await foundTask.remove();
+      res.send(foundTask);
     }
   } catch (error) {
     console.log(error);
@@ -62,12 +64,27 @@ router.delete("/delete/:task_id", async (req, res) => {
 router.put("/updateStage/:task_id/:stg", async (req, res) => {
   try {
     const task = await Task.findById(req.params.task_id);
-    console.log(task);
-    console.log(req.params.stg);
     task.stage = req.params.stg;
     await task.save();
+    res.send(task);
   } catch (error) {
     console.log(error);
   }
+});
+
+// @route  PUT api/tasks/updateTask/:task_id
+// @desc   update stage of a task
+// @access Public
+
+router.put("/updateTask/:task_id", async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.task_id, {
+    name: req.body.name,
+    assignee: req.body.assignee,
+    stage: req.body.stage,
+    desc: req.body.desc,
+    creationDate: req.body.creationDate,
+    dueDate: req.body.dueDate,
+  });
+  res.send(task);
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 
 //DB config
 const db = require("./config/keys").mongoURI;
+// const db = process.env.mongoURI;
 
 //connect to db
 mongoose
@@ -33,6 +34,6 @@ app.get("/", (req, res) => res.send("Hello"));
 app.use(cors());
 app.use("/api/tasks", tasks);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
